d2149e51f1c8535c6de53ded7cacdb64be8525ff
André Dâmaso <andre_damaso_1996@hotmail.com>
2020-07-25 01:34:50 +0100

    update

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.mail import send_mail, EmailMessage
 from django.conf import settings
 from django.template.loader import get_template  
@@ -47,7 +48,8 @@ def contact(request):
 
         # return render(request, 'contact.html', {'message_name': message_name})
         # return HttpResponse({'message_name': message_name})
-        return HttpResponse('')
+        #return HttpResponse('')
+        return HttpResponseRedirect(request.path)
         # return Http
     
     else:
